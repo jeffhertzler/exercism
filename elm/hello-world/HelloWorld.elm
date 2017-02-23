f54aa9b005df7b75f6f6788ee3dce7f888e64578
Jeff Hertzler <jeffhertzler@gmail.com>
2017-02-22 21:04:18 -0500

    elm - hello world

---
+++ elm/hello-world/HelloWorld.elm
@@ -0,0 +1,13 @@
+module HelloWorld exposing (..)
+
+import Array
+
+
+helloWorld : Maybe String -> String
+helloWorld name =
+    case name of
+        Nothing ->
+            "Hello, World!"
+
+        Just name ->
+            "Hello, " ++ name ++ "!"
