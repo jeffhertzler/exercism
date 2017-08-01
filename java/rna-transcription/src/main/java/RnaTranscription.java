5b83929bf279c5fe2db23e8e3ed1a99a5a84b9f2
Jeff Hertzler <jeffhertzler@gmail.com>
2017-08-01 08:05:04 -0400

    java - rna-transcription

---
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+public class RnaTranscription {
+  public String transcribe(String dnaStrand) {
+    if (dnaStrand == "") return "";
+    HashMap<String, String> translator = new HashMap<String, String>();
+    translator.put("G", "C");
+    translator.put("C", "G");
+    translator.put("T", "A");
+    translator.put("A", "U");
+    return Arrays.stream(dnaStrand.split(""))
+      .map(c -> translator.get(c))
+      .collect(Collectors.joining(""));
+  }
+}
