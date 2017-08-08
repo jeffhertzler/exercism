d7d683c5dfa5a2f43a5dd04ee168fc85d8417830
Jeff Hertzler <jeffhertzler@gmail.com>
2017-08-08 00:31:45 -0400

    javascript - leap

---
+++ javascript/leap/leap.js
@@ -0,0 +1,8 @@
+module.exports = class Year {
+  constructor(year) {
+    this.year = year;
+  }
+  isLeap() {
+    return this.year % 400 === 0 || (this.year % 4 === 0 && this.year % 100 !== 0);
+  }
+};
