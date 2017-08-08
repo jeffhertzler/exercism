b45db4b913de0b6a20e7688283378f97262949a2
Jeff Hertzler <jeffhertzler@gmail.com>
2017-08-08 03:33:35 -0400

    javascript - simple-cipher

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -1,28 +1,31 @@
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+
 const enc = (x, y) => x + y;
 const dec = (x, y) => x - y;
-const base = 'a'.charCodeAt(0);
-const diff = (key, i) => key.charCodeAt(i % key.length) - base;
-const constrain = x => base + (x - base) % 26;
-const codes = (str, key, f) => {
-  return str.split('').map((c, i) => {
-    const code = c.charCodeAt(0);
-    const newCode = f(code, diff(key, i));
-    return constrain(newCode);
-  });
+
+const index = c => letters.indexOf(c);
+
+const translate = (str, key, f) => {
+  return str
+    .split('')
+    .map((c, i) => {
+      const code = f(index(c), index(key[i]));
+      return letters[code % letters.length];
+    })
+    .join('');
 };
-const strFromCodes = args => String.fromCharCode(...args);
 
 module.exports = class Cipher {
   constructor(key = 'aaaaaaaaaa') {
-    if (key.match(/^[a-z]+$/) === null) {
+    if (!/^[a-z]+$/.test(key)) {
       throw new Error('Bad key');
     }
     this.key = key;
   }
   encode(str) {
-    return strFromCodes(codes(str, this.key, enc));
+    return translate(str, this.key, enc);
   }
   decode(str) {
-    return strFromCodes(codes(str, this.key, dec));
+    return translate(str, this.key, dec);
   }
 };
