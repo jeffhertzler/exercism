933bdb8dec4848e978c33aa38e7a96f219fc197e
Jeff Hertzler <jeffhertzler@gmail.com>
2018-08-04 15:18:18 -0400

    go - gigasecond - updated comments
    fixes golint issue

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// AddGigasecond adds 10^9 seconds to the passed in time.
 func AddGigasecond(t time.Time) time.Time {
 	return t.Add(time.Duration(math.Pow10(9)) * time.Second)
 }
