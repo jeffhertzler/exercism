07368ffd16e2f3459f0a851750bfcaa598dda09f
Jeff Hertzler <jeffhertzler@gmail.com>
2018-10-16 22:02:18 -0400

    go - gigasecond - simpler

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,11 +1,10 @@
 package gigasecond
 
 import (
-	"math"
 	"time"
 )
 
 // AddGigasecond adds 10^9 seconds to the passed in time.
 func AddGigasecond(t time.Time) time.Time {
-	return t.Add(time.Duration(math.Pow10(9)) * time.Second)
+	return t.Add(1e9 * time.Second)
 }
